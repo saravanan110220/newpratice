0d5e5412740dfd453eadfa28c8a322154f3ed7ff
saravanan110220 <saravanan.r2023a@vitstudent.ac.in>
2026-02-19 06:41:15 +0530

    initial commit=added firts application

---
+++ simple.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char operator;
+    float num1,num2;
+
+    printf("enter the operator");
+    scanf("%c",&operator);
+
+    printf("enter the two numbers");
+    scanf("%f %f",&num1,&num2);
+
+    switch(operator){
+        case '+' :
+            printf("result=%.2f",num1+num2);
+            break;
+
+        case '-' :
+           printf("result=%.2f",num1-num2); 
+           break;   
+         
+        default:
+            printf("inavlid operator");
+            break;
+
+    }
+
+    return 0;
+    
+}
